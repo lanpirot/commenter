4861657d0cc214cea366f0a6d9755c85367b9dde
Alexander Boll <boll@informatik.hu-berlin.de>
2023-06-06 18:06:28 +0200

    testing model 2718

--- enrich_models.m
+++ enrich_models.m
@@ -52,7 +52,7 @@ function all_projects = enrich(all_projects, en_tuple)
                 continue
             end
 
-            if (strcmp(en_tuple{1}, C.CYCLOMATIC_COMP) && ismember(model.m_num, [242, 399])) || ismember(model.m_num, [2718, 3705, 6150, 6151, 6807])
+            if (strcmp(en_tuple{1}, C.CYCLOMATIC_COMP) && ismember(model.m_num, [242, 399])) || ismember(model.m_num, [2718, 3705, 6150, 6151, 6807, 6808])
                 continue
             end
 
